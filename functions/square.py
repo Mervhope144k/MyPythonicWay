19a32f5b0f3f1711b82c2ba58a72b85443cccdd8
Merveilles <merveilleskatumba@gmail.com>
2024-01-09 09:38:45 -0500

    python functions of similar examples and more exercises

---
+++ functions/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import turtle
+
+def draw_square(t,sz):
+	""" Make turtle t draw a square of sz."""
+	for i in range(4):
+		t.forward(sz)
+		t.left(90)
+
+
+wn = turtle.Screen()	# set up the windown and its attributes
+wn.bgcolor("lightgreen")
+wn.title("Alex meets a function")
+
+alex = turtle.Turtle()	# Create alex
+draw_square(alex,50)	# Call the function to draw the square
+wn.mainloop()
